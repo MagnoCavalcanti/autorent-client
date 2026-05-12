f151833a1b50033a2bf66f0319198dadbe134a54
MagnoCavalcanti <magnocavalcanti9@gmail.com>
2026-05-12 14:13:32 -0300

    fix: muda a url para variável do .env local

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios, { type AxiosInstance, AxiosError, type InternalAxiosRequestConfig } from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api/v1';
+
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export const api: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
